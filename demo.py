6dfb365ee7273986a6e9e4b3b95cc90282e8757c
Willie Wheeler <willie.wheeler@gmail.com>
2024-10-04 01:16:16 -0700

    Refactored strand generation and translation

--- demo.py
+++ demo.py
@@ -1,18 +1,26 @@
 import random
+
+def generate_strand(length):
+    return ''.join([random.choice(nucleotides) for _ in range(length)])
+
+def translate_strand(strand):
+    typeshi = str.maketrans('ACTG', 'TGAC')
+    return strand.translate(typeshi)
+
 def basecount(chain):
     counts = {'A': 0, 'C': 0, 'T': 0, 'G': 0}
     for base in chain:
         counts[base] += 1
     return counts
+
 def convert(chain):
     RNA = chain.replace('T', 'U')
     return RNA
 
 nucleotides = ['A', 'C', 'T', 'G']
 length = int(input('How many base pairs would you like there to be? '))
-strand = ''.join([random.choice(nucleotides) for _ in range(length)])
-typeshi = str.maketrans('ACTG', 'TGAC')
-strand2 = strand.translate(typeshi)
+strand = generate_strand(length)
+strand2 = translate_strand(strand)
 
 counts = basecount(strand)
 print(strand)
